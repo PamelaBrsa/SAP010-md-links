d2b1d9064aa98d793659b5ee3f3afb3ef2f74529
PamelaBrsa <pamela.barbosa16@yahoo.com.br>
2023-08-09 11:58:40 -0300

    refatorando

--- index.js
+++ index.js
@@ -1,136 +1,75 @@
-const fs = require ('fs'); 
+const fs = require('fs');
 const fetch = require('cross-fetch')
 const path = require('path');
 
 
 
 
-function pegarLinks(data, diretorio) {
-  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
-  const capturarRegex = [...data.matchAll(regex)];
-  const resultado = capturarRegex.map((match) => ({
-    text: match[1],
-    href: match[2],
-    file: diretorio
-    
-  }));
-  return resultado;
-  }
-  
-  function arquivo(file) {
-    return new Promise((resolve, reject) => {
-      fs.promises.stat(file)
-        .then((stats) => resolve(stats.isDirectory()))
-        .catch(() => resolve(false));
+function validarCaminho (file) {
+  const validar = path.resolve(file);
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf-8', (error, conteudo) => {
+      if(error) {
+        if(!file.endsWith(".md")){
+          reject(new Error("Não foi encontrado um caminho md"));
+        } else {
+          reject(new Error("Erro ao ler o arquivo)"));
+        }
+        return
+        }
+
+        const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+        const capturarRegex = [...conteudo.matchAll(regex)];
+        const resultado = capturarRegex.map((match) => ({
+          text: match[1],
+          href: match[2],
+          file: validar,
+      
+        }));
+        resolve(resultado);
+      });
     });
   }
+
   
-  function mdlinks(file, options) {
-    return new Promise((resolve, reject) => {
-      arquivo(file)
-        .then((isDir) => {
-          if (isDir) {
-            fs.promises.readdir(file)
-              .then((files) => {
-                const promises = files.map((filename) => {
-                  const caminho = path.join(file, filename);
-                  return mdlinks(caminho, options);
-                });
-                Promise.all(promises)
-                  .then((resultados) => {
-                    const todosLinks = resultados.reduce((acc, links) => acc.concat(links), []);
-                    if (options.stats) {
-                      const stats = pegarEstatisticas(todosLinks);
-                      resolve({ links: todosLinks, stats });
-                    } else {
-                      resolve(todosLinks);
-                    }
-                  })
-                  .catch((error) => {
-                    reject(error);
-                  });
-              })
-              .catch((error) => {
-                reject(error);
-              });
-          } else {
-            const validarCaminho = path.extname(file).toLowerCase();
-            if (validarCaminho === '.md') {
-              fs.promises.readFile(file, 'utf-8')
-                .then((result) => {
-                  const links = pegarLinks(result, file);
-                  if (options.validate) {
-                    const solicitar = links.map((link) => validarLink(link));
-                    Promise.all(solicitar)
-                      .then((validatedLinks) => {
-                        if (options.stats) {
-                          const stats = pegarEstatisticas(validatedLinks);
-                          resolve({ links: validatedLinks, stats });
-                        } else {
-                          resolve(validatedLinks);
-                        }
-                      })
-                  } else {
-                    if (options.stats) {
-                      const stats = pegarEstatisticas(links);
-                      resolve({ links, stats });
-                    } else {
-                      resolve(links);
-                    }
-                  }
-                })
-                .catch(() => {
-                  console.log('caminho inexistente');
-                });
-            } else {
-              console.log('não é um caminho .md');
-            }
-          }
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
+  function validarLink (url,text, file) {
+
+    return fetch(url)
+    .then((response) => ({
+      href: url,
+      text: text,
+      file: file,
+      status: response.status ? "ok" : "error",
+      ok: response.ok ? "ok" : "fail",
+    }))
+    .catch((error) => ({
+      href: url,
+      text: text,
+      file: file,
+      status: "error",
+      ok: "fail",
+    }));
   }
-  
 
 
-  
-  function validarLink (link) {
-    //requisiçãp http para validar
-    
-    return fetch (link.href)
-
-    .then((resposta) =>{
-      link.status = resposta.status;
-      link.ok = 'OK';
-      if(link.status >= 400){
-        link.ok = 'FAIL';
+function mdlinks(file, options  = { validate: false }) {
+  const validar = path.resolve(file);
+  return validarCaminho(validar)
+     .then((links)=> {
+      if(options.validate){
+        let promises = links.map(link => validarLink(link.href, link.text, link.file));
+        return Promise.all(promises);
+      }else {
+        return links;
       }
-      return link;
-    })
-    .catch((erro)=> {
-      link.status = erro;
-      link.ok = 'FAIL';
-      return link;
-    })
-  }
-
-function pegarEstatisticas(links){
-  const total = links.length;
-  const exclusivo = [...new Set(links.map((link) => link.href))].length;
-  const quebrados = links.filter((link) => link.ok === 'FAIL').length;
-
-   return {
-    total: total,
-    exclusivo: exclusivo,
-    quebrados: quebrados
-  };
+     })
+     .catch ((error) => {
+      console.error(error)
+     });
 }
 
+  
 
+   
 
-
-//  mdLinks('./arquivos/teste.md', {validate: true})    
-
- module.exports = mdlinks
+                module.exports = { mdlinks, validarLink, validarCaminho }
